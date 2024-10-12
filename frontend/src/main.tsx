07fea7b02d6cad19269a052d07ee2b5f4fc89c6c
AngelynDisguise <angelyn.domingo122@gmail.com>
2024-10-12 19:34:42 -0400

    2nd merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import Results from "./components/Results.tsx";
+import Layout from "./components/Layout.tsx"
 import "./index.css";
 
 const router = createBrowserRouter([
