465d6527a67ad6ed871cecbbcff87000873c2c0d
user <ba789@protonmail.com>
2024-10-12 13:23:44 -0400

    Create AddPhoto component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,10 @@
-import { useState } from "react";
+import AddPhoto from "./components/AddPhoto";
 import "./App.css";
 
 function App() {
-  const [image, setImage] = useState(0);
-
-  const addImage = async () => {
-    //
-    // const file = event.target.files[0];
-  };
-
-  const submitImage = async () => {
-    if (!image) {
-      return;
-    }
-    let formData = new FormData();
-    formData.append("file", image);
-    let response = await fetch("http://localhost:3000/food", {
-      method: "GET",
-      body: formData,
-    });
-    if (response.status === 200) {
-      // navigate(`/food/${foodName}`);
-    }
-  };
-
   return (
     <>
-      <div>
-        <button onClick={addImage}>Add food image {image}</button>
-      </div>
-
-      {image != 0 && (
-        <div>
-          <button onClick={submitImage}>Submit</button>
-        </div>
-      )}
+      <AddPhoto />
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
+import App from "./App.tsx";
+import Layout from "./components/Layout.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
